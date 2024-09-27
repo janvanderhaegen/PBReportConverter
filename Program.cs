fcaae54bfd6d21178463a5d6296cd5d8edcd7e6d
Jan Van der Haegen <jvh@trilogyes.com>
2024-09-27 15:37:30 +0200

    Program now either a) converts all .pbl files to repx files, OR b) converts all .srd files to .repx files. Trying everything in one go is just too much (during early development stages)

--- Program.cs
+++ Program.cs
@@ -3,25 +3,17 @@ using ReportMigration.Converters;
 
 Console.WriteLine("Enter path of directory with PowerBuilder files:");
 var pbPath = Console.ReadLine();
+
+//check if input directory exists
 var pbInfo = new DirectoryInfo(pbPath!);
 if (!pbInfo.Exists)
 {
-    Console.WriteLine("Directory doesn't exist");
+    Console.WriteLine("Input directory doesn't exist");
     return;
 }
-pbPath = pbInfo.FullName;
+pbPath = pbInfo.FullName; 
 
-
-Console.WriteLine("Enter path of target directory:");
-var repxPath = Console.ReadLine();
-var repxInfo = new DirectoryInfo(repxPath!);
-if (!repxInfo.Exists)
-{
-    Console.WriteLine("Directory doesn't exist");
-    return;
-}
-repxPath = repxInfo.FullName;
-
+//if there are any pbl files, convert them
 var pebbles = Directory.GetFiles(pbPath!, "*.pbl", SearchOption.AllDirectories);
 Console.WriteLine($"Found {pebbles.Length} pbl files");
 var pblToSrdConverter = new PblToSrdConverter();
@@ -29,10 +21,22 @@ foreach (var pebble in pebbles)
 {
     pblToSrdConverter.Unpack(pebble);
 }
+if (pebbles.Any())
+    return;
+
+//else, convert srd files to repx
+Console.WriteLine("Enter path of target directory:");
+var repxPath = Console.ReadLine();
+var repxInfo = new DirectoryInfo(repxPath!);
+if (!repxInfo.Exists)
+{
+    repxInfo.Create();
+}
+repxPath = repxInfo.FullName;
 
 
 var srdFiles = new[] { "*.srd", "*.p" }.SelectMany(pattern => Directory.GetFiles(pbPath!, pattern, SearchOption.AllDirectories)).ToArray();
-Console.WriteLine($"Found {srdFiles.Length} .srd/.p files");
+Console.WriteLine($"Found {srdFiles.Length} .srd|.p files");
 var converter = new SrdToRepxConverter(pbPath!, repxPath!);
 foreach (var fileName in srdFiles)
 {
