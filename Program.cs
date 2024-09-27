4e7c3a7bf27bdb1a574a897dacdf0d5e98d37a1d
Jan Van der Haegen <jvh@trilogyes.com>
2024-09-27 13:52:47 +0200

    Program now automatically unpacks pebbles (.pbl files)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿using ReportMigration.Converters;
+﻿using PBReportConverter.Converters;
+using ReportMigration.Converters;
 
 Console.WriteLine("Enter path of directory with PowerBuilder files:");
 var pbPath = Console.ReadLine();
+var pbInfo = new DirectoryInfo(pbPath!);
+if (!pbInfo.Exists)
+{
+    Console.WriteLine("Directory doesn't exist");
+    return;
+}
+pbPath = pbInfo.FullName;
+
 
 Console.WriteLine("Enter path of target directory:");
 var repxPath = Console.ReadLine();
+var repxInfo = new DirectoryInfo(repxPath!);
+if (!repxInfo.Exists)
+{
+    Console.WriteLine("Directory doesn't exist");
+    return;
+}
+repxPath = repxInfo.FullName;
+
+var pebbles = Directory.GetFiles(pbPath!, "*.pbl", SearchOption.AllDirectories);
+Console.WriteLine($"Found {pebbles.Length} pbl files");
+var pblToSrdConverter = new PblToSrdConverter();
+foreach (var pebble in pebbles)
+{
+    pblToSrdConverter.Unpack(pebble);
+}
 
 var files = Directory.GetFiles(pbPath!).Select(f => Path.GetFileName(f));
 
