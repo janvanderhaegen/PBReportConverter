b64f2be1ded8146b882fd7bf96b87436e728fe28
Jan Van der Haegen <jvh@trilogyes.com>
2024-10-16 06:19:38 +0200

    RepxToJson: tool can now be pointed to a folder with repx files to turn them into JSON files that can be saved to the TIES Resource Center (TFS)

--- Program.cs
+++ Program.cs
@@ -2,19 +2,19 @@
 using ReportMigration.Converters;
 
 Console.WriteLine("Enter path of directory with PowerBuilder files:");
-var pbPath = Console.ReadLine();
+var inputPath =Console.ReadLine();
 
 //check if input directory exists
-var pbInfo = new DirectoryInfo(pbPath!);
+var pbInfo = new DirectoryInfo(inputPath!);
 if (!pbInfo.Exists)
 {
     Console.WriteLine("Input directory doesn't exist");
     return;
 }
-pbPath = pbInfo.FullName; 
+inputPath = pbInfo.FullName;
 
 //if there are any pbl files, convert them
-var pebbles = Directory.GetFiles(pbPath!, "*.pbl", SearchOption.AllDirectories);
+var pebbles = Directory.GetFiles(inputPath!, "*.pbl", SearchOption.AllDirectories);
 Console.WriteLine($"Found {pebbles.Length} pbl files");
 var pblToSrdConverter = new PblToSrdConverter();
 foreach (var pebble in pebbles)
@@ -22,24 +22,47 @@ foreach (var pebble in pebbles)
     pblToSrdConverter.Unpack(pebble);
 }
 if (pebbles.Any())
+{
+    Console.WriteLine($"Unpacked {pebbles.Length} pbl files");
     return;
+}
 
 //else, convert srd files to repx
 Console.WriteLine("Enter path of target directory:");
-var repxPath = Console.ReadLine();
-var repxInfo = new DirectoryInfo(repxPath!);
+var outputPath = Console.ReadLine();
+var repxInfo = new DirectoryInfo(outputPath!);
 if (!repxInfo.Exists)
 {
     repxInfo.Create();
 }
-repxPath = repxInfo.FullName;
+outputPath = repxInfo.FullName;
 
 
-var srdFiles = new[] { "*.srd", "*.p" }.SelectMany(pattern => Directory.GetFiles(pbPath!, pattern, SearchOption.AllDirectories)).ToArray();
+var srdFiles = new[] { "*.srd", "*.p" }.SelectMany(pattern => Directory.GetFiles(inputPath!, pattern, SearchOption.AllDirectories)).ToArray();
 Console.WriteLine($"Found {srdFiles.Length} .srd|.p files");
-var converter = new SrdToRepxConverter(pbPath!, repxPath!);
+var converter = new SrdToRepxConverter(inputPath!, outputPath!);
 foreach (var fileName in srdFiles)
 {
-    var relativePath = Path.GetRelativePath(pbPath!, fileName);
+    var relativePath = Path.GetRelativePath(inputPath!, fileName);
     converter.GenerateRepxFile(relativePath);
+}
+if (srdFiles.Any())
+{
+    Console.WriteLine($"Converted {srdFiles.Length} srd files to repx");
+    return;
+}
+
+//else, convert repx files to json
+var repxFiles = Directory.GetFiles(inputPath!, "*.repx", SearchOption.AllDirectories).ToArray();
+Console.WriteLine($"Found {repxFiles.Length} .repx files");
+var repxToJsonConverter = new RepxToJsonConverter(inputPath!, outputPath!);
+foreach (var fileName in repxFiles)
+{
+    var relativePath = Path.GetRelativePath(inputPath!, fileName);
+    repxToJsonConverter.ConvertToJson(relativePath);
+}
+if (repxFiles.Any())
+{
+    Console.WriteLine($"Converted {repxFiles.Length} repx files to json");
+    return;
 }
